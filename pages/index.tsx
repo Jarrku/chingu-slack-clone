fd7866785aba09aec7d7b9e411f448bceb2574ff
Jarrku <simonvdbroeck@gmail.com>
2018-04-23 00:38:11 +0200

    Provide SSR apollo client helper

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import withData from "./lib/withData";
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `;
 
-export default () => <Title>Hello World!</Title>;
+export default withData(() => <Title>Hello World!</Title>);
