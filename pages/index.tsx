e3cbde1613a8dae0073368cf9d81bcfd67ecc0b2
Simon VDB <simonvdbroeck@gmail.com>
2018-04-24 00:44:19 +0200

    Add GrphQL intro to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 import withData from "./lib/withData";
+import { PingQuery, query } from "../graphql/pingQuery";
 
 const Title = styled.h1`
   color: red;
   font-size: 50px;
 `;
+interface Props {
+  title: string;
+}
 
-export default withData(() => <Title>Hello World!</Title>);
+class Index extends React.Component<Props> {
+  static getInitialProps(): Props {
+    return {
+      title: "MyTitle",
+    };
+  }
+
+  render() {
+    const { title } = this.props;
+
+    return (
+      <PingQuery query={query}>
+        {({ data, loading, error }) => (
+          <React.Fragment>
+            <Title>{title}</Title>
+            <p>{data.ping}</p>
+          </React.Fragment>
+        )}
+      </PingQuery>
+    );
+  }
+}
+
+export default withData(Index);
